c52a6db63d6913c94b6ce81e671458f2cee8b7be
Aarya Bhatia <aarya@Aaryas-MacBook-Pro.local>
2021-06-27 10:07:07 +0530

    ready for deploy

--- config/s3.config.js
+++ config/s3.config.js
@@ -4,15 +4,6 @@ const aws = require("aws-sdk");
 const s3 = new aws.S3();
 
 const bucket = process.env.AWS_BUCKET_NAME;
-// const region = process.env.AWS_BUCKET_REGION;
-// const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-
-// aws.config.update({
-//   secretAccessKey,
-//   accessKeyId,
-//   region,
-// });
 
 const fileFilter = function (req, file, cb) {
   const filetypes = /jpeg|jpg|png/;
