3bfd86a3c3362848b9d16a1b2911a6aa9bd890d7
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2021-08-08 12:42:40 +0530

    refactor

--- controller/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    AuthController: require('./auth'),
-    DbController: require('./db.controller'),
-    UserAssessmentController: require('./scorer/assessment'),
-    UserProfileController: require('./user.profile'),
-    UtilController: require('./util')
-}
--- routers/index.router.js
+++ routers/index.router.js
@@ -1,9 +1,28 @@
-const { getAllAssessments } = require("../controller/assessments");
 const { sendEmail } = require("../services/send.email");
+const { Cateogry, Assessment } = require("../models");
 const router = require("express").Router();
 
 // Home page
-router.get("/", getAllAssessments);
+router.get("/", async (req, res) => {
+  const categories = await Category.find({});
+
+  const data = [];
+
+  for (const category of categories) {
+    const assessments = await Assessment.find({ category_id: category._id });
+
+    data.push({
+      category_name: category.name,
+      category_description: category.description,
+      assessments,
+    });
+  }
+
+  res.render("index", {
+    data,
+    ...res.locals,
+  });
+});
 
 // Contact us
 router.get("/contact-us", (req, res) => {
