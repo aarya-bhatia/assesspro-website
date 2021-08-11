a2af802298e38599fc67f8b3e2f5bf1d4ada9163
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2021-08-11 07:49:06 +0530

    put contact us router in different file

---
+++ routers/contact.router.js
@@ -0,0 +1,46 @@
+const { sendEmail } = require("../services/send.email");
+const router = require("express").Router();
+
+// Contact us
+router.get("/", (req, res) => {
+  res.render("contact.us.ejs", {
+    loggedIn: res.locals.loggedIn,
+    success: null,
+    error: null,
+  });
+});
+
+// Send Message
+router.post("/", async (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(req.body);
+  try {
+    sendEmail(
+      process.env.CONTACT_US_EMAIL,
+      "Someone sent a message!",
+      {
+        name,
+        email,
+        message,
+      },
+      "./template/contact.us.ejs"
+    ).then(() => {
+      console.log("email sent.");
+    });
+
+    res.render("contact.us.ejs", {
+      loggedIn: res.locals.loggedIn,
+      success: "Message sent successfully...",
+      error: null,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("contact.us.ejs", {
+      loggedIn: res.locals.loggedIn,
+      success: null,
+      error: err.message,
+    });
+  }
+});
+
+module.exports = router;
